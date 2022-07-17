9a5265888afd8b1f3c94a6a246ce7707c474d7bb
Eneko9 <enekoeguigurenm@gmail.com>
2022-07-17 12:47:01 +0200

    Contract deployment to local blockchain (ganache)

--- deploy.js
+++ deploy.js
@@ -1,5 +1,29 @@
+const ethers = require("ethers");
+const fs = require("fs-extra");
+
 async function main() {
-  
+  //http://127.0.0.1:7545
+
+  //connection to local blockchain
+  const provider = new ethers.providers.JsonRpcProvider(
+    "http://127.0.0.1:7545"
+  );
+  //caution with showing the provate key
+  const wallet = new ethers.Wallet(
+    "f27c93fe0674fd62a6c90bbbc61ebcb4e2707ded2bfc19ae653ebc308480a0b8",
+    provider
+  );
+
+  const abi = fs.readFileSync("./SimpleStorage_sol_SimpleStorage.abi", "utf8");
+  const binary = fs.readFileSync(
+    "./SimpleStorage_sol_SimpleStorage.bin",
+    "utf8"
+  );
+
+  const contractFactory = new ethers.ContractFactory(abi, binary, wallet);
+  console.log("Deploying...");
+  const contract = await contractFactory.deploy(); //await --> Stop here until the deployment done
+  console.log(contract);
 }
 
 main()
